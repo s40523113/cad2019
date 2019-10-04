6b1711a27a7a421b491eb1c5a5c24c2aa2b43e76
s40523113 <40523113@gm.nfu.edu.tw>
2019-10-04 22:35:13 +0800

    add1

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cad2019/content/index.html \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cad2019/content/index.html \xa0 \n', 'tags': '', 'url': 'About.html'}]};
